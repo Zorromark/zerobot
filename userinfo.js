8d2ab20d6edeacd6651c74acd28c2440e4815ff6
Zero <141378709+Zorromark@users.noreply.github.com>
2024-05-06 16:49:46 -0700

    userinfo command

--- userinfo.js
+++ userinfo.js
@@ -1,23 +1,31 @@
+// Updated userinfo.js
+
 const { EmbedBuilder } = require("discord.js");
 
 const command = {
   name: 'userinfo',
   description: 'Get information about a user.',
-  execute: async function(message) {
-    const user = message.author;
+  execute: async function(message, mentionedUser) {
+    const user = mentionedUser || message.author;
 
-    const joinDateDiscord = user.createdAt;
-    const joinDateServer = message.guild.members.cache.get(user.id).joinedAt;
+    let userToFind = message.mentions.users.first();
+    if (!userToFind) {
+      const userId = message.content.split(' ')[1];
+      userToFind = message.guild.members.cache.get(userId);
+    }
+
+    const joinDateDiscord = userToFind.createdAt;
+    const joinDateServer = message.guild.members.cache.get(userToFind.id).joinedAt;
 
     const userinfoEmbed = new EmbedBuilder()
       .setColor('#0099ff')
       .setTitle('User Information')
-      .setDescription(`Username: ${user.username}\nID: ${user.id}`)
+      .setDescription(`Username: ${userToFind.username}\nID: ${userToFind.id}`)
       .addFields(
         { name: 'Joined Discord', value: joinDateDiscord.toUTCString() },
         { name: 'Joined Server', value: joinDateServer.toUTCString() }
       )
-      .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+      .setThumbnail(userToFind.displayAvatarURL({ dynamic: true }))
       .setTimestamp();
 
     message.channel.send({ embeds: [userinfoEmbed] });
