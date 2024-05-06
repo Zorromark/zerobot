36d252625120f2b47a9ebc762e940549ba97b12c
Zero <141378709+Zorromark@users.noreply.github.com>
2024-05-06 16:50:06 -0700

    upd

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 const Discord = require('discord.js');
-const { Client, GatewayIntentBits, ActivityType, AttachmentBuilder, Partials, Args } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, MessageEmbed, Embuilder } = require('discord.js');
 require('dotenv/config');
-
-const { createCanvas, loadImage } = require('canvas');
-const crypto = require('crypto');
-const { MessageEmbed }  = require('discord.js')
-const { EmbedBuilder } = require('discord.js')
-const { MessageAttachment } = require('discord.js');
-const { ApplicationCommandType, ApplicationCommandOptionType } = require('discord.js');
-const { ButtonStyle } = require('discord.js');
 const userinfo = require('./userinfo.js');
-
-
 
 const client = new Client({
   intents: [
@@ -21,14 +11,19 @@ const client = new Client({
     GatewayIntentBits.MessageContent
   ]
 });
+
+client.on('error', (error) => {
+    console.error('An error occurred:', error);
+});
+
 client.commands = new Map();
 
 client.on('ready', () => {
-    console.log('Bot is ready to rick and roll')
-    client.user.setStatus('available')
-    client.user.setActivity(
-        'r!help',
-        { type: ActivityType.Playing })
+    console.log('Bot is ready to rick and roll');
+    client.user.setStatus('available');
+    client.user.setActivity('r!help', { type: ActivityType.Playing });
+});
+
 
 const canvacord = require ("canvacord")
 
@@ -49,9 +44,9 @@ client.on('guildMemberAdd', async member => {
   member.guild.channels.cache.get("1138967181162467420").send({ embeds: [welcomeEmbed], files: [attachment] });
 });
 
-})
+
 function serverInfo(guild){
-    return guild.available ? new EmbedBuilder()
+    return guild.available ? new Discord.EmbedBuilder()
     .setTitle(`Server information for ${guild.name} (${guild.id})`)
     .setDescription(
         `Creation Time: ${guild.createdAt}`
@@ -60,66 +55,54 @@ function serverInfo(guild){
     .setDescription('error, try again later');
 }
 
-
 client.on('messageCreate', (message) => {
   if (message.content === 'r!ping') {
     const pingTime = Date.now() - message.createdTimestamp;
-    const pingEmbed = new EmbedBuilder()
+    const pingEmbed = new Discord.EmbedBuilder()
       .setColor('#0099ff')
       .setTitle('Ping')
       .setDescription(`Pong! Bot latency is ${pingTime}ms.`);
-
     message.channel.send({ embeds: [pingEmbed] });
   }
 
-  if (message.content ==='r!help') {
-    const embuilder = new EmbedBuilder()
-    .setTitle("Zerobot help")
+  if (message.content === 'r!help') {
+    const embuilder = new Discord.EmbedBuilder()
+      .setTitle("Zerobot help")
       .setDescription(`Zerobot is a moderation bot used in many occasions for moderating servers and is going to be an outstanding bot in the future
-
-          Commands:
-          \`help\`
-          \`ping\`
-          \`serverinfo\`
-          \`welcomecardguild\`
-          \`invite\`
-          \`userinfo\`
-          \`kick\`
-          `)
-    .setColor('#eb2617');
-    message.channel.send({embeds:[embuilder]});
-
+      Commands:
+      \`help\`
+      \`ping\`
+      \`serverinfo\`
+      \`welcomecardguild\`
+      \`invite\`
+      \`userinfo\`
+      \`kick\`
+      `)
+      .setColor('#eb2617');
+    message.channel.send({ embeds: [embuilder] });
   }
-  if (message.content ==='r!invite') {
-    const embuilder = new EmbedBuilder()
+
+  if (message.content === 'r!invite') {
+    const embuilder = new Discord.EmbedBuilder()
       .setTitle("Invite Zerobot")
-      .setDescription(`https://discord.com/api/oauth2/authorize?client_id=1110068022976192513&permissions=8&scope=bot`)
+      .setDescription(`[Click here](https://discord.com/api/oauth2/authorize?client_id=1110068022976192513&permissions=8&scope=bot)`)
       .setColor('#8f2121');
-    message.channel.send({ embeds: [ embuilder ] });
+    message.channel.send({ embeds: [embuilder] });
   }
 
   if (message.content === 'r!serverinfo') {
-    message.channel.send({ embeds: [ serverInfo(message.guild) ] });
+    message.channel.send({ embeds: [serverInfo(message.guild)] });
   }
 
   client.commands.set(userinfo.name, userinfo);
-  if (message.content === 'r!userinfo') {
-    const args = message.content.split(' ');
-    const command = args.shift().slice('r!'.length);
+  if (message.content.startsWith('r!userinfo')) {
+    const mentionedUser = message.mentions.users.first();
+    client.commands.get('userinfo').execute(message, mentionedUser);
+  }
 
-    if (client.commands.has(command)) {
-      try {
-        client.commands.get(command).execute(message);
-      } catch (error) {
-        console.error(error);
-        message.reply("There was an error executing that command.");
   
-      } 
-      }
-    }
   const kickCommand = require('./kick.js');
   client.commands.set(kickCommand.name, kickCommand);
 });
-  }
-);
+
 client.login(process.env.token);
